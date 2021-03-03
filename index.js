159983276ccc9bfececa2a01a6bc266195a29973
mike_shih <mike_shih@mail.chungyo.net>
2021-03-03 14:12:45 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-recess-order',
+        'stylelint-config-prettier'
+    ],
+    plugins: ['stylelint-scss'],
+    rules: {
+        'color-hex-case': 'lower',
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true,
+        'font-family-no-missing-generic-family-keyword': null,
+        'selector-pseudo-class-no-unknown': [true, {
+            ignorePseudoClasses: ['deep', 'v-deep']
+        }],
+        'no-empty-source': null
+    }
+};
